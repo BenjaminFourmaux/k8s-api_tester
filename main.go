602226b807efc6910a3ccc38308f3fafd5031cff
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2023-10-29 01:02:55 +0200

    Fix Dockerfile

--- main.go
+++ main.go
@@ -14,15 +14,17 @@ func main() {
 	/* - Startup - */
 	Console.Write("K8s - API Tester")
 	Console.Write("version : 0.0.1")
-	Console.Write("source : https://github.com/BenjaminFourmaux/k8s_api-tester")
+	Console.Write("source : https://github.com/BenjaminFourmaux/k8s-api_tester")
 	Console.Write("author : Benjamin Fourmaux -- Beruet")
 	Console.Write("license : Apache 2.0")
 	Console.Write("--------------------")
 
 	/* -- Read Tests manifest YAML file  -- */
-	yamlFileTests, err := os.ReadFile("Resource/tests.yaml")
+	//yamlFileTests, err := os.ReadFile("Resource/tests.yaml")
+	yamlFileTests, err := os.ReadFile("/etc/api_tester/tests.yaml")
 	if err != nil {
 		Console.Error("Unable to read Test config YAML file")
+		panic(err)
 	}
 
 	var testConfig Entity.TestConfig
@@ -31,6 +33,7 @@ func main() {
 
 	if err != nil {
 		Console.Error("Unable to parse in YAML. Check syntax")
+		panic(err)
 	}
 
 	/* --- Test Pipeline --- */
