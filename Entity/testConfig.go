85428abef265d3bd2370f9d6cbc957ddf0d449f9
BenjaminFourmaux <benjamin.fourmauxb@gmail.com>
2023-11-12 19:13:57 +0100

    Add Header parameters

--- Entity/testConfig.go
+++ Entity/testConfig.go
@@ -6,8 +6,9 @@ type TestConfig struct {
 
 // Describe a Test
 type Test struct {
-	Name             string // Represent the Name of the test. Ex: Internet connection
-	Url              string // The HTTP url of the API
-	Method           string // Wich HTTP Method do you want use send request
-	ExpectedHTTPCode int    `yaml:"expectedHTTPCode"` // The expected HTTP code
+	Name             string   // Represent the Name of the test. Ex: Internet connection
+	Url              string   // The HTTP Url of the API
+	Method           string   // Wich HTTP Method do you want use send request
+	ExpectedHTTPCode int      `yaml:"expectedHTTPCode"` // The expected HTTP code
+	Headers          []string `yaml:"headers,flow"`     // List of HTTP Headers
 }
